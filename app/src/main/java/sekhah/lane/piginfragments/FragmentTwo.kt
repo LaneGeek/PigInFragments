30666d912a412ad770cbe2a818b2f766599a39dd
HS <sekhah@my.lanecc.edu>
2019-11-02 14:14:48 -0700

    Game works now. Still a long way to go...

--- app/src/main/java/sekhah/lane/piginfragments/FragmentTwo.kt
+++ app/src/main/java/sekhah/lane/piginfragments/FragmentTwo.kt
@@ -10,6 +10,9 @@ import kotlinx.android.synthetic.main.fragment2.*
 
 class FragmentTwo : Fragment() {
 
+    private var pigGame = PigGame()
+    private var die = 0
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment2, container, false)
     }
@@ -20,8 +23,62 @@ class FragmentTwo : Fragment() {
         player1NameTextView.text = player1
         player2NameTextView.text = player2
 
-        rollDieButton.setOnClickListener() {
-            Toast.makeText(activity, "Roll", Toast.LENGTH_SHORT).show()
+        rollDieButton.setOnClickListener {
+            die = pigGame.rollDie()
+            if (die == 1) {
+                rollDieButton.isEnabled = false
+                turnButton.text = "End Turn"
+                pigGame.changeTurn()
+            }
+            updateScreen()
+        }
+
+        turnButton.setOnClickListener {
+            if (turnButton.text == "Start Turn") {
+                turnButton.text = "End Turn"
+                rollDieButton.isEnabled = true
+            } else {
+                turnButton.text = "Start Turn"
+                rollDieButton.isEnabled = false
+                pigGame.changeTurn()
+                die = 0
+            }
+            updateScreen()
+        }
+
+        updateScreen()
+    }
+
+    private fun updateScreen() {
+        if (pigGame.currentPlayer == 1)
+            nextTurnTextView.text = "${player1NameTextView.text}'s Turn"
+        else
+            nextTurnTextView.text = "${player2NameTextView.text}'s Turn"
+
+        player1ScoreTextView.text = pigGame.player1Score.toString()
+        player2ScoreTextView.text = pigGame.player2Score.toString()
+        turnPointsTextView.text = pigGame.turnPoints.toString()
+
+        // Check for winner
+        if (pigGame.checkForWinner() != -1) {
+            when (pigGame.checkForWinner()) {
+                0 -> nextTurnTextView.text = "It is a tie!"
+                1 -> nextTurnTextView.text = "${player1NameTextView.text} Wins!"
+                2 -> nextTurnTextView.text = "${player2NameTextView.text} Wins!"
+            }
+            rollDieButton.isEnabled = false
+            turnButton.isEnabled = false
+        }
+
+        // Update the die image
+        when (die) {
+            0 -> dieImageView.setImageResource(R.drawable.pig)
+            1 -> dieImageView.setImageResource(R.drawable.die1)
+            2 -> dieImageView.setImageResource(R.drawable.die2)
+            3 -> dieImageView.setImageResource(R.drawable.die3)
+            4 -> dieImageView.setImageResource(R.drawable.die4)
+            5 -> dieImageView.setImageResource(R.drawable.die5)
+            6 -> dieImageView.setImageResource(R.drawable.die6)
         }
     }
 }
