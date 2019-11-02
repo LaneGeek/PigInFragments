40a2809a6b03ca26dae2ca689d65fdc8829abb8d
HS <sekhah@my.lanecc.edu>
2019-11-02 14:59:42 -0700

    Tidy up.

--- app/src/main/java/sekhah/lane/piginfragments/FragmentOne.kt
+++ app/src/main/java/sekhah/lane/piginfragments/FragmentOne.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment1.*
 
@@ -20,7 +19,6 @@ class FragmentOne : Fragment() {
             val player2 = player2EditText.text.toString()
             (activity as MainActivity).playerNames(player1, player2)
             (activity as MainActivity).newGame()
-            Toast.makeText(activity, "New Game", Toast.LENGTH_SHORT).show()
         }
     }
 }
