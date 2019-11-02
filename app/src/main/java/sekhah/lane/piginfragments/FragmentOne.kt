633a54dfe46e739c356f4d397bf87efffecea155
HS <sekhah@my.lanecc.edu>
2019-11-02 11:19:40 -0700

    Initial commit

---
+++ app/src/main/java/sekhah/lane/piginfragments/FragmentOne.kt
@@ -0,0 +1,22 @@
+package sekhah.lane.piginfragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment1.*
+
+class FragmentOne : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        newGameButton.setOnClickListener() {
+            Toast.makeText(activity, "New Game", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
