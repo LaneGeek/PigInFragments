8ba5b15e9752dd1beb4529651cea42bb5036ee20
HS <sekhah@my.lanecc.edu>
2019-11-02 20:45:25 -0700

    Landscape layout working.

--- app/src/main/java/sekhah/lane/piginfragments/MainActivity.kt
+++ app/src/main/java/sekhah/lane/piginfragments/MainActivity.kt
@@ -3,22 +3,37 @@ package sekhah.lane.piginfragments
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class MainActivity : AppCompatActivity(), IPig {
 
     private var player1Name = ""
     private var player2Name = ""
+    private var onePaneLayout = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Using Brian's hack to find out if one or two panes are running
+        onePaneLayout = fragment2 == null
     }
 
     fun newGame() {
-        val intent = Intent(this, SecondActivity::class.java)
-        intent.putExtra("player1Name", player1Name)
-        intent.putExtra("player2Name", player2Name)
-        startActivity(intent)
+        if (onePaneLayout) {
+            // Start SecondActivity
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("player1Name", player1Name)
+            intent.putExtra("player2Name", player2Name)
+            startActivity(intent)
+        } else {
+            // Launch FragmentTwo within this activity
+            val frag = FragmentTwo()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment2, frag)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
     }
 
     fun playerNames(player1Name: String, player2Name: String) {
